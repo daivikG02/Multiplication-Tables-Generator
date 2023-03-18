b2b4b4c9192dedfaa2a34f52b0a1c8a02a11977c
daivikgarg2 <128111734+daivikG02@users.noreply.github.com>
2023-03-18 15:53:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("This is a  program to generate multiplication tables ")
+n=int(input("Enter the number for printing out its table: "))
+
+for i in range(1,11): 
+  print(n," x ",i," = ", i*n) 
